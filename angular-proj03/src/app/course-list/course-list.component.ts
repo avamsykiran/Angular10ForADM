5387d0541978d245409d8cb1c3b83d61c092c741
Aripaka Vamsy Kiran <a.vamc.it@gmail.com>
2021-02-12 14:38:54 +0530

    day 9

--- angular-proj03/src/app/course-list/course-list.component.ts
+++ angular-proj03/src/app/course-list/course-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CourseService } from '../service/course.service';
+import { StudentService } from '../service/student.service';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-course-list',
@@ -10,9 +12,16 @@ import { CourseService } from '../service/course.service';
 export class CourseListComponent implements OnInit {
 
   courses:Course[];
+  infoMsg:string;
   errMsg:string;
+  showActions:boolean;
 
-  constructor(private courseService:CourseService) { }
+  constructor(
+    private courseService:CourseService,
+    private userService:UserService,
+    private studentService:StudentService) {
+    this.showActions = (userService.isLoggedIn() && userService.currentUser().role=="STUDENT");
+   }
 
   ngOnInit(): void {
     this.courseService.getAll().subscribe(
@@ -23,5 +32,40 @@ export class CourseListComponent implements OnInit {
         this.errMsg=err;
       }
     );
+  }
+  
+  enroll(cid:number){
+
+    let uid = this.userService.currentUser().userId;
+
+    this.studentService.getById(parseInt(uid)).subscribe(
+      (student) =>{
+        
+        if(!student.enrollments){
+          student.enrollments=[];
+        }
+
+        student.enrollments.push(
+          {
+            id:student.enrollments.length+1,
+            courseId:cid,
+            studentId:student.id,
+            dateOfEnrollment:new Date()
+          }
+        );
+
+        this.studentService.modify(student,student.id).subscribe(
+          (student) => {
+            this.infoMsg="Youy have enrolled to the selected course";
+          },
+          (err) => {
+            this.errMsg=err;
+          }
+        );
+      },
+      (err) => {
+        this.errMsg=err;
+      }
+    );    
   }
 }
