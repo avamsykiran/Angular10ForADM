53e1b35aefe90611e1a2a500f23ddd2c3dec5da8
Aripaka Vamsy Kiran <a.vamc.it@gmail.com>
2021-02-12 16:22:53 +0530

    day9

--- angular-proj03/src/app/enrollments/enrollments.component.ts
+++ angular-proj03/src/app/enrollments/enrollments.component.ts
@@ -10,21 +10,58 @@ import { UserService } from '../service/user.service';
 })
 export class EnrollmentsComponent implements OnInit {
 
-  enrollments:Enrollment[];
+  //enrollments:Enrollment[];
+  enrollViews: EnrollmentView[];
 
   constructor(
-    private studentService:StudentService,
-    private userService:UserService
+    private studentService: StudentService,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
     let uid = this.userService.currentUser().userId;
 
     this.studentService.getById(parseInt(uid)).subscribe(
-      (student) =>{
-        this.enrollments=student.enrollments;
+      (student) => {
+        this.enrollViews = student.enrollments.map(
+          (e) => ({ enrollment: e, isSelected: false })
+        );
       }
     );
   }
 
+  toggleSelection(e: EnrollmentView) {
+    e.isSelected = !e.isSelected;
+  }
+
+  discardSelected() {
+    let uid = this.userService.currentUser().userId;
+
+    this.studentService.getById(parseInt(uid)).subscribe(
+      (student) => {
+        this.enrollViews.forEach(
+          ev => {
+            if (ev.isSelected) {
+              let index = student.enrollments.findIndex(e => e.id == ev.enrollment.id);
+              student.enrollments = student.enrollments.splice(index, 1);
+            }
+          }
+        );
+        this.studentService.modify(student,student.id).subscribe(
+          (data) => {
+            this.loadData();
+          }
+        );
+      }
+    );
+  }
+}
+
+class EnrollmentView {
+  enrollment: Enrollment;
+  isSelected: boolean;
 }
